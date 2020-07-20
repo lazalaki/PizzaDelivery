d9dcb918b8765c6042eafc5d06fc226794dfef50
lazalaki <lakierbes@gmail.com>
2020-07-21 00:22:03 +0200

    Updates

---
+++ precache-manifest.0a6e0fe037ebf14ef521f69395ee21ab.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd30c4f5cdd9b80826b9dd58c5b2866",
+    "url": "/PizzaDelivery/index.html"
+  },
+  {
+    "revision": "e9e09bddaa6f44f2c674",
+    "url": "/PizzaDelivery/static/css/2.2a8a6f76.chunk.css"
+  },
+  {
+    "revision": "ffbfd12c20ea513581cf",
+    "url": "/PizzaDelivery/static/css/main.43b3970f.chunk.css"
+  },
+  {
+    "revision": "e9e09bddaa6f44f2c674",
+    "url": "/PizzaDelivery/static/js/2.a9ba06b1.chunk.js"
+  },
+  {
+    "revision": "80231e00a1e8ff0d55f518c6b3d4fe7e",
+    "url": "/PizzaDelivery/static/js/2.a9ba06b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffbfd12c20ea513581cf",
+    "url": "/PizzaDelivery/static/js/main.528b290f.chunk.js"
+  },
+  {
+    "revision": "e6645e43f0d76a289cfe",
+    "url": "/PizzaDelivery/static/js/runtime-main.9b0a56f6.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/PizzaDelivery/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/PizzaDelivery/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/PizzaDelivery/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/PizzaDelivery/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/PizzaDelivery/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/PizzaDelivery/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/PizzaDelivery/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/PizzaDelivery/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/PizzaDelivery/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/PizzaDelivery/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/PizzaDelivery/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/PizzaDelivery/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/PizzaDelivery/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/PizzaDelivery/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/PizzaDelivery/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/PizzaDelivery/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/PizzaDelivery/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "159263bf4976843830365d52d1f94fba",
+    "url": "/PizzaDelivery/static/media/primeicons.159263bf.eot"
+  },
+  {
+    "revision": "2e3a6b48408add8bb604d0b4e820a3b5",
+    "url": "/PizzaDelivery/static/media/primeicons.2e3a6b48.ttf"
+  },
+  {
+    "revision": "4e915114c908962a6ff2d505c2a4816b",
+    "url": "/PizzaDelivery/static/media/primeicons.4e915114.svg"
+  },
+  {
+    "revision": "79432094e4838b36d974b9d8c57d22e2",
+    "url": "/PizzaDelivery/static/media/primeicons.79432094.woff"
+  }
+]);
